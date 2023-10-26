04f744839890de41fa310a12cd285260243a51a1
yogita-s-24 <yogitashete24@gmail.com>
2023-10-26 16:03:27 +0530

    feat : post api created to add user

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+import User from "./models/User.js";
+
 const app = express();
 
 app.use(express.json());
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const connectMongoDB = async () => {
     const conn = await mongoose.connect(MONGODB_URI);
@@ -17,6 +19,45 @@ const connectMongoDB = async () => {
     }
   };
   connectMongoDB();
+
+
+  //Post/signup
+
+  app.post('/signup', async (req,res)=>{
+    const {name, email, password, mobile, address, gender} = req.body;
+
+    //instance 
+    const user = new User({
+      name :name,
+      email:email,
+      password:password,
+      mobile:mobile,
+      address:address,
+      gender:gender 
+    })
+
+    try{
+      const savedUser = await user.save();
+      res.json({
+        success : true,
+        data :savedUser,
+        message:"SignUp Successful"
+      });
+
+    }
+
+    catch(err){
+      res.json({
+        success : false,
+        message : email.message
+      })
+    }
+  })
+
+
+
+  //port 
+  const PORT = process.env.PROT || 5000;
 
   app.listen(PORT, ()=>{
     console.log('Server is running on port 5000');
