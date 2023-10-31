453074a152a141d559c0fa5a57af5bdfcfd25a6d
yogita-s-24 <yogitashete24@gmail.com>
2023-10-31 10:25:31 +0530

    feat - coustom css added

--- client/src/views/Signup/Signup.js
+++ client/src/views/Signup/Signup.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import showToast from "crunchy-toast";
 import "./Signup.css";
+import SignUpImg from "./../Signup/shopping.png";
 
 function SignUp() {
   const [name, setName] = useState("");
@@ -12,7 +13,6 @@ function SignUp() {
   const [gender, setGender] = useState("female");
 
   async function signupUser() {
-
     if (!name) {
       showToast("name is required", "alert", 5000);
       return;
@@ -43,15 +43,11 @@ function SignUp() {
     });
     console.log(response.data);
 
-
-    if (response.data.success) 
-    {
+    if (response.data.success) {
       showToast(response.data.message, "success", 3000);
 
       window.location.href = "/login";
-    }
-     else
-      {
+    } else {
       showToast(response.data.message, "alert", 3000);
       setName("");
       setEmail("");
@@ -62,112 +58,114 @@ function SignUp() {
   }
   return (
     <>
-    <section class="form mx-5 my-4 ">
-        <div class="container ">
-          <div class="row  border shadow no-gutters">
+      <section class="form mx-5 my-4 ">
+        <div class="container">
+          <div class="row border shadow no-gutters">
             <div class="col-lg-6">
-              {/* <img
-                src={SignupImg}
+              <img
+                src={SignUpImg}
                 alt="dog"
-                class="img-fluid d-block mx-auto"
-                style={{ width: "500px" }}
-              /> */}
+                class="img-fluid d-block mx-auto mt-5"
+                style={{ width: "450px" }}
+              />
             </div>
-            <div class="col-lg-6 pt-5 px-5"></div>
 
-            <h2 className="text-center">SignUp </h2>
-                <form>
-                  <div className="mt-3">
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="fullName"
-                      placeholder="Enter your Name"
-                      value={name}
-                      onChange={(e) => setName(e.target.value)}
-                    />
-                  </div>
-                  <div className="mt-3">
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="email"
-                      placeholder="Enter your Email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
-                  </div>
-                  <div className="mt-3">
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="password"
-                      placeholder="Enter your password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                  </div>
-                  <div className="mt-3">
-                    <input
-                      type="phone"
-                      className="form-control"
-                      id="mobile"
-                      placeholder="Enter your mobile number"
-                      value={mobile}
-                      onChange={(e) => setMobile(e.target.value)}
-                    />
-                  </div>
-                  <div className="mt-3">
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="address"
-                      placeholder="Enter your Address"
-                      value={address}
-                      onChange={(e) => setAddress(e.target.value)}
-                    />
-                  </div>
-                  <div className="d-flex justify-content-evenly">
-                    <div>
-                      <input
-                        type="radio"
-                        id="male"
-                        name="gender"
-                        className="mt-3"
-                        checked={gender === "male"}
-                        onClick={() => {
-                          setGender("male");
-                        }}
-                      />
-                      <label htmlFor="male">Male</label>
-                    </div>
-
-                    <div className="mt-3">
-                      <input
-                        type="radio"
-                        id="female"
-                        name="gender"
-                        className="gender"
-                        checked={gender === "female"}
-                        onClick={() => {
-                          setGender("female");
-                        }}
-                      />
-                      <label htmlFor="female">Female</label>
-                    </div>
-                  </div>
-
-                  <button
-                    type="button"
-                    className="btn btn-primary  my-5 px-4 d-block mx-auto"
-                    onClick={signupUser}>
-                    <b>SignUp</b>
-                  </button>
-                </form>
+            <div class="col-lg-6 pt-5 px-5">
+              <h1 className="text-center"><i>"Buy-Bye-Bliss"</i> </h1>
+              <h2 className="text-center text-secondary">SignUp Here </h2>
+              <form>
+                <div className="mt-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="fullName"
+                    placeholder="Enter your Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
                 </div>
-          </div>
-      </section>
+                <div className="mt-3">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    placeholder="Enter your Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="mt-3">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <div className="mt-3">
+                  <input
+                    type="phone"
+                    className="form-control"
+                    id="mobile"
+                    placeholder="Enter your mobile number"
+                    value={mobile}
+                    onChange={(e) => setMobile(e.target.value)}
+                  />
+                </div>
+                <div className="mt-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="address"
+                    placeholder="Enter your Address"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                  />
+                </div>
+                <div className="d-flex justify-content-evenly">
+                  <div>
+                    <input
+                      type="radio"
+                      id="male"
+                      name="gender"
+                      className="mt-3"
+                      checked={gender === "male"}
+                      onClick={() => {
+                        setGender("male");
+                      }}
+                    />
+                    <label htmlFor="male">Male</label>
+                  </div>
+
+                  <div className="mt-3">
+                    <input
+                      type="radio"
+                      id="female"
+                      name="gender"
+                      className="gender"
+                      checked={gender === "female"}
+                      onClick={() => {
+                        setGender("female");
+                      }}
+                    />
+                    <label htmlFor="female">Female</label>
+                  </div>
+                </div>
+
+                <button
+                  type="button"
+                  className="btn btn-primary  my-3 px-4 d-block mx-auto"
+                  onClick={signupUser}>
+                  <b>SignUp</b>
+                </button>
+              </form>
+              </div>
             
+          </div>
+        </div>
+      </section>
     </>
   );
 }
